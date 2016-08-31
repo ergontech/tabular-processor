4582e2762dbb4d417809a925af8a28ea3b01fb27
Matthew Wells <matthewpaulwells@gmail.com>
2016-08-31 16:38:10 -0700

    Update GoogleSheetsLoadStep.php
    Add some inline documentation

--- src/GoogleSheetsLoadStep.php
+++ src/GoogleSheetsLoadStep.php
@@ -55,11 +55,14 @@ class GoogleSheetsLoadStep implements Step
     {
         $headers = $this->sheetsService->spreadsheets_values->get($this->sheetId, $this->headerRangeName)->getValues();
         $values = $this->sheetsService->spreadsheets_values->get($this->sheetId, $this->dataRangeName)->getValues();
+        // Google sends the *minimum* back. Meaning if there are "trailing NULL entries" those won't be included!
+        // However, it's very important to our processor that column count matches the count of the columns in each row
+        // Thus, we fill!
         $fill = array_fill(0, count($headers[0]), null);
         return $next(new Rows(
             $headers[0],
             array_map(function ($data) use($fill) {
-                return $data + $fill;
+                return $data + $fill; // For numeric arrays, the plus operator works like array_merge works for associative arrays. _However_ the order is reversed. /shrug
             }, $values)));
     }
 }
